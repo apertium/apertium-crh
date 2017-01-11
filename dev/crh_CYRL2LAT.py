3524af0c9618fe8f60e00058a63e89fccb5845e0
Francis M. Tyers <ftyers@prompsit.com>
2017-01-11 23:55:42 +0000

    fix

--- dev/crh_CYRL2LAT.py
+++ dev/crh_CYRL2LAT.py
@@ -1,7 +1,59 @@
 #!/usr/bin/env python3
 
-def doStuff:
-	print("hargle")
+import sys;
 
-if __name__ == "__main__":
-	doStuff()
+tt = {
+	'къ':'q',
+	'нъ':'ñ',
+	'гъ':'ğ',
+	'дж':'c',
+	'а':'a',
+	'б':'b',
+	'в':'v',
+	'г':'g',
+	'д':'d',
+	'е':'e',
+	'ё':'yo',
+	'ж':'j',
+	'з':'z',
+	'и':'i',
+	'й':'y',
+	'к':'k',
+	'л':'l',
+	'м':'m',
+	'н':'n',
+	'o':'o',
+	'п':'p',
+	'p':'r',
+	'c':'s',
+	'т':'t',
+	'у':'u',
+	'ф':'f',
+	'x':'h',
+	'ц':'ts',
+	'ч':'ç',
+	'ш':'ş',
+	'щ':'ş',
+	'ъ':'',
+	'ы':'ı',
+	'ь':'',
+	'э':'e',
+	'ю':'yu',
+	'я':'ya'
+};
+
+def main(i, o, tt): #{
+	k = list(tt.keys());
+	for line in i.readlines(): #{
+		for s in sorted(tt, key=len, reverse=True): #{
+			line = line.replace(s, tt[s]);
+			line = line.replace(s.upper(), tt[s].upper());
+		#}
+		o.write(line);
+	#}	
+	
+#}
+
+if __name__ == "__main__": #{
+	main(sys.stdin, sys.stdout, tt);
+#}
