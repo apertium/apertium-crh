729249d4f481e9b2a91a314f8af9a24f531ab7cd
Francis M. Tyers <ftyers@prompsit.com>
2017-01-12 02:50:28 +0000

    ere

--- dev/crh_CYRL2LAT.sh
+++ dev/crh_CYRL2LAT.sh
@@ -3,6 +3,6 @@ for i in `cat crh_CYRL2LAT.twol | grep '!!' | sed 's/ /_/g' | grep -v '^$'`; do
 	w=`echo $i | sed 's/_/ /g' | sed 's/!! //g' | sed 's/ //g'`; 
 	z=`echo $w | cut -f1 -d':' | hfst-strings2fst | hfst-compose-intersect -2 crh_CYRL2LAT.hfst | hfst-fst2strings`; 
 	if [[ $w != $z ]]; then
-		echo "$z";
+		echo "$w != $z";
 	fi
 done
