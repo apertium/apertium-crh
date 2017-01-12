e4ad12b7b142a023b9985de2890a5805c3e1c5c6
Francis M. Tyers <ftyers@prompsit.com>
2017-01-12 04:11:15 +0000

    update makefile

--- dev/crh_CYRL2LAT.sh
+++ dev/crh_CYRL2LAT.sh
@@ -1,6 +1,7 @@
 echo ""
 for i in `cat crh_CYRL2LAT.twol | grep '!!' | sed 's/ /_/g' | grep -v '^$'`; do 
 	w=`echo $i | sed 's/_/ /g' | sed 's/!! //g' | sed 's/ //g'`; 
+	echo "=== "$w" ========================================"
 	z=`echo $w | cut -f1 -d':' | hfst-strings2fst | hfst-compose-intersect -2 crh_CYRL2LAT.hfst | hfst-fst2strings`; 
 	if [[ $w != $z ]]; then
 		echo "$z != $w";
@@ -9,5 +10,6 @@ done
 echo ""
 for i in `cat crh_CYRL2LAT.twol | grep '!@' | sed 's/ /_/g' | grep -v '^$'`; do
 	w=`echo $i | sed 's/!@_//g' | sed 's/_/ /g'`;
+	echo "=== "$w" ========================================"
 	echo $w | hfst-pair-test crh_CYRL2LAT.hfst;
 done
